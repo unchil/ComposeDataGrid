9fadd600661563f4d90ff260e6b2691accc9706c
unchil <yeopo77@icloud.com>
2025-12-24 13:57:23 +0900

    Initial commit

--- un7datagrid/src/commonMain/kotlin/com/unchil/un7datagrid/Un7KCMPSearchMenu.kt
+++ un7datagrid/src/commonMain/kotlin/com/unchil/un7datagrid/Un7KCMPSearchMenu.kt
@@ -17,7 +17,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ManageSearch
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
@@ -35,7 +35,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
@@ -53,7 +52,6 @@ internal fun Un7KCMPSearchMenu(
     var expanded by remember { mutableStateOf(false) }
     val filterText = remember { mutableStateOf("") }
     val operatorText = remember { mutableStateOf(OperatorMenu.Operators.first().toString()) }
-    var isFocused by remember { mutableStateOf(false) }
     val scrollState = remember { ScrollState(0) }
     var expandedOperator by remember { mutableStateOf(false) }
 
@@ -142,25 +140,22 @@ internal fun Un7KCMPSearchMenu(
                                 isSearch.value = true
                                 true
                             } else false
-                        }
-                        .onFocusChanged { focusState ->  isFocused = focusState.isFocused  },
+                        } ,
                     value = filterText.value,
                     onValueChange = { filterText.value = it  },
                     label = { Text("Search...")  },
                     trailingIcon = {
                         IconButton(
                             onClick = {
-                                isSearch.value = true
-                            },
-                            enabled = isFocused,
+                                filterText.value = ""
+                            }
                         ) {
-                            Icon(Icons.Default.Search,
-                                contentDescription = "Search",
-                                tint = if (isFocused) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.inverseOnSurface
+                            Icon(Icons.Default.Clear,
+                                contentDescription = "Clear",
+                                tint =  MaterialTheme.colorScheme.onSurface
                             )
                         }
                     },
-
                     singleLine = true,
                     keyboardOptions = KeyboardOptions(
                         imeAction = ImeAction.Search
